c939249b0e81bb8f70a0348f3e1e35e126205b59
Toma Apostolov & Carlo De Gregorio <student@epicodus.com>
2015-08-05 16:35:29 -0700

    Passed a test of taking a string and replacing a word in that string with a different word

--- lib/find_and_replace.rb
+++ lib/find_and_replace.rb
@@ -1,6 +1,9 @@
 class String
-  define_method(:find_and_replace) do
-    word = "Hello"
-    new_word = word.replace "World"
+  define_method(:find_and_replace) do |word, replace|
+    phrase = self
+
+    if(phrase.include?(word))
+      return phrase.gsub word, replace
+    end
   end
 end
--- spec/find_and_replace_spec.rb
+++ spec/find_and_replace_spec.rb
@@ -1,8 +1,14 @@
 require('rspec')
 require('find_and_replace')
+require('pry')
 
 describe('String#find_and_replace') do
   it('takes a word and sreplaces it with a different one.') do
-      expect('Hello'.find_and_replace()).to(eq('World'))
+      expect('Hello'.find_and_replace("Hello", "World")).to(eq('World'))
   end
+
+  it('takes a string and replaces a word in that string with a different word') do
+      expect("Hello World".find_and_replace("World", "Universe")).to(eq("Hello Universe"))
+
+    end
 end
